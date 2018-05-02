361df34226dea1baa2ab4703b49a065de55e2e50
Wafaa S Taie <wstaie@wstaie-mac01.local>
2018-05-02 12:08:04 -0700

    Use a different approach to delete leftover namespaces after tests are done, some code review changes.

--- tests/e2e/test_deploy_flow.py
+++ tests/e2e/test_deploy_flow.py
@@ -19,36 +19,14 @@
 #
 import os
 import pytest
-import uuid
-import getpass
-from mlt.utils.process_helpers import run
+
 from test_utils.e2e_commands import CommandTester
 from test_utils.files import create_work_dir
 
 
-# Expose the namespace value per test to be used in tearing down 
-# the remaining resources, important to have in case tests fail.
-def app_parameters(workdir):
-    # setup the test session parameters
-    app_name = str(uuid.uuid4())[:10]
-    app_params = {
-        'app_name': app_name,
-        'workdir': workdir,
-        'namespace': getpass.getuser() + '-' + app_name
-    }
-    return app_params
-
-# tear down remaining resources
-def sess_teardown(namespace):
-    try:
-        run(["kubectl", "--namespace", namespace, "delete", "-f", "k8s"])
-    except SystemExit:
-    #this means that the namespace and k8s resources are already deleted 
-        pass
-
-
 # NOTE: you need to deploy first before you deploy with --no-push
 # otherwise you have no image to use to make new container from
+
 
 @pytest.mark.parametrize('template',
                          filter(lambda x: os.path.isdir(
@@ -56,35 +34,32 @@ def sess_teardown(namespace):
                              os.listdir('mlt-templates')))
 def test_simple_deploy(template):
     with create_work_dir() as workdir:
-        app_params = app_parameters(workdir)
-        commands = CommandTester(app_params)
+        commands = CommandTester(workdir)
         commands.init(template)
         commands.build()
         commands.deploy()
         commands.undeploy()
-        sess_teardown(app_params['namespace'])
+        commands.teardown()
 
 
 def test_no_push_deploy():
     with create_work_dir() as workdir:
-        app_params = app_parameters(workdir)
-        commands = CommandTester(app_params)
+        commands = CommandTester(workdir)
         commands.init()
         commands.build()
         commands.deploy()
         commands.deploy(no_push=True)
         commands.undeploy()
-        sess_teardown(app_params['namespace'])
+        commands.teardown()
 
 
 def test_watch_build_and_deploy_no_push():
     with create_work_dir() as workdir:
-        app_params = app_parameters(workdir)
-        commands = CommandTester(app_params)
+        commands = CommandTester(workdir)
         commands.init()
         commands.build(watch=True)
         commands.deploy()
         commands.deploy(no_push=True)
         commands.undeploy()
-        sess_teardown(app_params['namespace'])
+        commands.teardown()
 
